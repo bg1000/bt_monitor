fa921af39bd1c7d1e942b5f589a9463c695d2abb
Bob <bobgray1000@gmail.com>
2019-11-20 17:46:12 +0000

    updated service files

--- autostart_systemd.sh
+++ autostart_systemd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp garageqtpi@pi.service /etc/systemd/system/garageqtpi@${SUDO_USER:-${USER}}.service
-sed -i "s?/home/pi/GarageQTPi?`pwd`?" /etc/systemd/system/garageqtpi@${SUDO_USER:-${USER}}.service
+cp bt_monitor@pi.service /etc/systemd/system/bt_monitor@${SUDO_USER:-${USER}}.service
+sed -i "s?/home/pi/bt_monitor?`pwd`?" /etc/systemd/system/bt_monitor@${SUDO_USER:-${USER}}.service
 systemctl --system daemon-reload
-systemctl enable garageqtpi@${SUDO_USER:-${USER}}.service
+systemctl enable bt_monitor@${SUDO_USER:-${USER}}.service
+systemctl start bt_monitor@pi
--- make_json.py
+++
@@ -1,9 +0,0 @@
-import json
-message = {
-"cmd": "scan", 
-"DeviceName": "Bob_Phone", 
-"Address": "A0:10:81:64:E7:93"
-}
-my_json = json.dumps(message)
-print(str(my_json))
-
