a2c35e91c832e8119ded4c1d42e8bb99d0b9c2be
Bob Gray <bobgray1000@gmail.com>
2019-12-06 00:53:56 +0000

    update to deployment scripts

--- deploy_bt_monitor.sh
+++ deploy_bt_monitor.sh
@@ -6,18 +6,20 @@ apt-get upgrade -y
 echo installing pi-bluetooth
 apt-get install pi-bluetooth
 echo installing mosquitto v1.6.4-0
-# get repo key
+#get repo key
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 #download appropriate lists file 
 cd /etc/apt/sources.list.d/
-wget http://repo.mosquitto.org/debian/mosquitto-stretch.list
+wget http://repo.mosquitto.org/debian/mosquitto-buster.list
 #update caches and install 
 apt-cache search mosquitto
 sudo apt-get update
-sudo apt-get install -f libmosquitto-dev=1.6.4-0 mosquitto=1.6.4-0 mosquitto-clients=1.6.4-0 libmosquitto1=1.6.4-0
-echo holding mosquitto version at 1.6.4-0
+sudo apt-get install -f libmosquitto-dev mosquitto mosquitto-clients libmosquitto1 -y
+# echo holding mosquitto version at 1.5.7-1
 sudo apt-mark hold libmosquitto-dev mosquitto mosquitto-clients libmosqu
+echo installing pip for python 3
+apt-get install python3-pip -y
 echo installing required python packages
-pip3 install -r requirements.txt
+pip3 install -r /home/pi/bt_monitor/requirements.txt
 echo setting up bt_monitor to run as a service
 sudo bash /home/pi/bt_monitor/autostart_systemd.sh 
