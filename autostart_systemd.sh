dc5eb1b8cdeb2863bcb8a347a4c673403bb7ef3f
bg1000 <38083150+bg1000@users.noreply.github.com>
2019-12-06 09:35:02 -0500

    incorporated directly into deploy_bt_monitor.sh

--- autostart_systemd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp bt_monitor@pi.service /etc/systemd/system/bt_monitor@${SUDO_USER:-${USER}}.service
-sed -i "s?/home/pi/bt_monitor?`pwd`?" /etc/systemd/system/bt_monitor@${SUDO_USER:-${USER}}.service
-systemctl --system daemon-reload
-systemctl enable bt_monitor@${SUDO_USER:-${USER}}.service
-systemctl start bt_monitor@pi
